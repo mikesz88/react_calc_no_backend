e6c4a8ee346563d2daeb3ce73241df7b2c9df336
michael <michaelsanchez8815@gmail.com>
2023-02-19 20:53:45 -0600

    setup history state

--- src/components/Calculator/CalcContainer.jsx
+++ src/components/Calculator/CalcContainer.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { INIT_STATE } from '../../constants/CalculatorState'
 
 const CalcContainer = () => {
+  const [state, setState] = useState(INIT_STATE);
+
+  const updateHistory = (equation) => {
+    setState(prevState => ({
+      ...prevState,
+      history: [...prevState.history, equation],
+    }))
+  }
+
   return (
     <div>CalcContainer</div>
   )
---
+++ src/constants/CalculatorState.js
@@ -0,0 +1,13 @@
+export const INIT_STATE = {
+  history: [],
+};
+
+/* 
+  Example object:
+  {
+  num1: X,
+  operation1: Y,
+  num2: Z,
+  Answer: A
+  }
+*/
